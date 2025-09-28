dcc5e1cdee6b711c4dbd214207d5ba9e9447f7ac
Gonzalo Argüello <arguellogonzalo97@gmail.com>
2025-09-28 02:34:47 -0300

    chore: add header.tsx component

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="text-center relative">
+      <h1 className="text-5xl font-bold tracking-tight">To-Do App</h1>
+      <button className="absolute top-0 right-0">Iniciar Sesión</button>
+    </header>
+  );
+};
+
+export default Header;
