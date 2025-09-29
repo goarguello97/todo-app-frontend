c6fc6e328fe6a4bce82011988c5793802ed339d0
Gonzalo Argüello <arguellogonzalo97@gmail.com>
2025-09-29 15:52:27 -0300

    refactor: adjust padding layout in card and tabs
    - created a "xs" brakpoint for tailwindcss

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,10 +5,10 @@ import TabsComponent from "./Tabs";
 
 const CardComponent = () => {
   return (
-    <div className="grid grid-rows-[150px_1fr] h-screen justify-items-center p-4 sm:p-8 md:p-12 lg:p-24">
+    <div className="grid grid-rows-[150px_1fr] h-screen justify-items-center xs:p-2 md:p-12 lg:p-24">
       <Header />
       <main className="flex w-full max-w-7xl overflow-auto flex-col">
-        <Card className="w-full max-w-7xl h-full min-h-full border !border-[#212529] !border-t-0 !rounded-lg !rounded-t-none shadow-sm p-4 sm:p-8 md:p-12 lg:p-24">
+        <Card className="w-full max-w-7xl h-full min-h-full border !border-[#212529] !border-t-0 !rounded-lg !rounded-t-none shadow-sm xs:p-2 sm:p-8 md:p-12 lg:p-24">
           <AddTask />
           <TabsComponent />
         </Card>
--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -5,29 +5,53 @@ const TabsComponent = () => {
   return (
     <Tabs
       defaultActiveKey="all"
-      className="mb-3 pt-4 w-100 items-center justify-around"
+      className="mb-3 pt-4 w-100 justify-around flex-nowrap"
     >
-      <Tab eventKey="all" title="Todas" className="flex-grow text-start">
-        <span className="text-3xl font-bold tracking-tight">Pendientes</span>
+      <Tab
+        eventKey="all"
+        title={
+          <span className="text-xs sm:text-sm md:text-base lg:text-lg font-medium">
+            Todas
+          </span>
+        }
+        className="flex-grow text-start"
+      >
+        <span className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold tracking-tight">
+          Pendientes
+        </span>
         <hr />
         <Task />
-        <span className="text-3xl font-bold tracking-tight">Completadas</span>
+        <span className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold tracking-tight">
+          Completadas
+        </span>
         <hr />
       </Tab>
       <Tab
         eventKey="pending"
-        title="Pendientes"
+        title={
+          <span className="text-xs sm:text-sm md:text-base lg:text-lg font-medium">
+            Pendientes
+          </span>
+        }
         className="flex-fill text-start"
       >
-        <span className="text-3xl font-bold tracking-tight">Pendientes</span>
+        <span className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold tracking-tight">
+          Pendientes
+        </span>
         <hr />
       </Tab>
       <Tab
         eventKey="completes"
-        title="Completadas"
+        title={
+          <span className="text-xs sm:text-sm md:text-base lg:text-lg font-medium">
+            Completadas
+          </span>
+        }
         className="flex-fill text-start"
       >
-        <span className="text-3xl font-bold tracking-tight">Completadas</span>
+        <span className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold tracking-tight">
+          Completadas
+        </span>
         <hr />
       </Tab>
     </Tabs>
