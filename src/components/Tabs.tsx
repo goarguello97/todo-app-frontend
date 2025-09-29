7225b8ccd086aa725ce684f213b66d96690a5502
Gonzalo Argüello <arguellogonzalo97@gmail.com>
2025-09-29 15:29:12 -0300

    feat: start task.tsx component
    -add react-icons library

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,4 +1,5 @@
 import { Tab, Tabs } from "react-bootstrap";
+import Task from "./Task";
 
 const TabsComponent = () => {
   return (
@@ -6,17 +7,29 @@ const TabsComponent = () => {
       defaultActiveKey="all"
       className="mb-3 pt-4 w-100 items-center justify-around"
     >
-      <Tab eventKey="all" title="Todas" className="flex-grow text-center"></Tab>
+      <Tab eventKey="all" title="Todas" className="flex-grow text-start">
+        <span className="text-3xl font-bold tracking-tight">Pendientes</span>
+        <hr />
+        <Task />
+        <span className="text-3xl font-bold tracking-tight">Completadas</span>
+        <hr />
+      </Tab>
       <Tab
         eventKey="pending"
         title="Pendientes"
-        className="flex-fill text-center"
-      ></Tab>
+        className="flex-fill text-start"
+      >
+        <span className="text-3xl font-bold tracking-tight">Pendientes</span>
+        <hr />
+      </Tab>
       <Tab
         eventKey="completes"
         title="Completadas"
-        className="flex-fill text-center"
-      ></Tab>
+        className="flex-fill text-start"
+      >
+        <span className="text-3xl font-bold tracking-tight">Completadas</span>
+        <hr />
+      </Tab>
     </Tabs>
   );
 };
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,13 +1,28 @@
 import { Toast } from "react-bootstrap";
+import { FaCheck } from "react-icons/fa";
 
 const Task = () => {
   return (
-    <Toast>
+    <Toast className="!w-full mb-3">
       <Toast.Header>
         <strong className="me-auto">Tarea</strong>
-        <small>{new Date().toISOString()}</small>
+        <small>
+          {new Date().toLocaleDateString("es-ES", {
+            weekday: "long",
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+          })}
+        </small>
       </Toast.Header>
-      <Toast.Body>Esta es una tarea de prueba.</Toast.Body>
+      <div className="flex items-center">
+        <Toast.Body className="text-start flex-grow">
+          Esta es una tarea de prueba.
+        </Toast.Body>
+        <button className="!m-[12px]">
+          <FaCheck />
+        </button>
+      </div>
     </Toast>
   );
 };
