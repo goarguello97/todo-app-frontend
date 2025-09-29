7ab7876a7eea50d6a73087793f01ef10d28abcd4
Gonzalo Argüello <arguellogonzalo97@gmail.com>
2025-09-29 11:56:40 -0300

    feat: start card.tsx component
    -initial structure, still incomplete

---
+++ src/components/Card.tsx
@@ -0,0 +1,7 @@
+import Card from "react-bootstrap/Card";
+
+const CardComponent = () => {
+  return <Card className="w-[90vw] min-h-[50vh]"></Card>;
+};
+
+export default CardComponent;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import CardComponent from "../components/Card";
 import Header from "../components/Header";
 
 const Home = () => {
   return (
     <main>
       <Header />
+      <CardComponent />
     </main>
   );
 };
