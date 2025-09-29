e48de48ca331da04c0a9c4714735ae829246a52e
Gonzalo Argüello <arguellogonzalo97@gmail.com>
2025-09-29 11:41:09 -0300

    refactor: update header.tsx component
    -adjust font sizes and colors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,14 +4,14 @@ const Header = () => {
   return (
     <Navbar expand="lg" className="bg-[#007BFF]">
       <Container>
-        <Navbar.Brand className="!text-3xl font-bold tracking-tight">
+        <Navbar.Brand className="!text-5xl font-bold tracking-tight">
           To-Do App
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-end">
           <Nav>
             <Nav.Link>
-              <button className="py-2 px-4 rounded bg-[#FFFFFF] hover:bg-[#212529] hover:text-[#E9ECEF] text-[#212529] border border-[#0056B3] hover:border-[#495057]">
+              <button className="py-2 px-4 rounded bg-[#FFFFFF] hover:bg-[#212529] hover:text-[#E9ECEF] text-[#495057] border-1 border-[#6C757D] hover:border-[#FFFFFF]">
                 Iniciar Sesión
               </button>
             </Nav.Link>
